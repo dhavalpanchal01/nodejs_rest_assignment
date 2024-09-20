8d6311769876b9035553e6768f5b85661c9cff92
dhavalpancal01 <dhvlpncl01@gmail.com>
2024-09-21 04:20:17 +0530

    created user model, routes, controllers

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ const app: Application = express();
 app.use(cors())
 app.use(express.json());
 
+
+import userRoutes from './routes/user.routes'
+app.use('/api/users', userRoutes)
+
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
     return res.send('Server is up!')
 })
